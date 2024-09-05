e421985551aeb74c95c7bf6a7d670c7911af084f
23DP2RTrei <166109672+23DP2RTrei@users.noreply.github.com>
2024-09-05 09:41:59 +0000

    uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,10 +1,17 @@
-package lv.rvt;
+package lv.rvt; //klases atrasanas vieta projekta
+
+import java.util.Scanner;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        
+        //Mes gribam uzinat no lietotaja vaaru uzvardu uzardu un grupu
+
+        //Un beigās printet visu rindiņā izmantoot string savienošanu
+
 
     }
 
