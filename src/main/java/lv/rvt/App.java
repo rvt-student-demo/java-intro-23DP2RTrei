7e16733ffc39afbfe887d4cc2025457ac5a9d35d
23DP2RTrei <166109672+23DP2RTrei@users.noreply.github.com>
2024-09-25 10:18:26 +0000

    Number and sum of numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,34 +5,21 @@ import java.util.*;
 public class App 
 {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Give me a number: ");
+    int num = scan.nextInt();
+    int count = 0;
+    int sum = num;
 
-        // Ievadam dāvanas vērtību
-        System.out.print("Ievadiet dāvanas vērtību: ");
-        double davanasVertiba = scanner.nextDouble();
-        double nodoklis = 0;
-
-        // Nodokļa aprēķins atkarībā no vērtības
-        if (davanasVertiba < 5000) {
-            System.out.println("Nodoklis nav jāmaksā!");
-        } else if (davanasVertiba <= 25000) {
-            nodoklis = 100 + (davanasVertiba - 5000) * 0.08;
-            System.out.println("Nodoklis: " + nodoklis);
-        } else if (davanasVertiba <= 55000) {
-            nodoklis = 1700 + (davanasVertiba - 25000) * 0.10;
-            System.out.println("Nodoklis: " + nodoklis);
-        } else if (davanasVertiba <= 200000) {
-            nodoklis = 4700 + (davanasVertiba - 55000) * 0.12;
-            System.out.println("Nodoklis: " + nodoklis);
-        } else if (davanasVertiba <= 1000000) {
-            nodoklis = 22100 + (davanasVertiba - 200000) * 0.15;
-            System.out.println("Nodoklis: " + nodoklis);
-        } else {
-            nodoklis = 142100 + (davanasVertiba - 1000000) * 0.17;
-            System.out.println("Nodoklis: " + nodoklis);
-        }
-
-        scanner.close();
+    while (num != 0) {
+        System.out.println("Give me a number: ");
+        num = scan.nextInt();
+        count++;
+        sum += num;
+    }
+    System.out.println("Number of numbers: " + count );
+    System.out.println("Sum of the numbers: " + sum );
+        
     }
 }
