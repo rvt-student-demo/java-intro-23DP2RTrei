0636b056c9d2db4cee280ec559d6505cda2fb90b
23DP2RTrei <166109672+23DP2RTrei@users.noreply.github.com>
2024-10-02 10:57:02 +0000

    From one to parameter

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,25 +5,15 @@ import java.util.*;
 public class App 
 {
 
-    public static void main(String[] args){
-    Scanner scanner = new Scanner(System.in);
-    int sum = 0;
-    System.out.println("Ievadiet skaitli: ");
-    int i = scanner.nextInt();
-    int numnum = 0;
-    float avg = 0;
-    while (i!=-1) {
+        public static void main(String[] args){
+            printUntil(5); 
+        }
 
-        int num = scanner.nextInt();
-        sum = sum + i; 
-        i = num;
-        numnum += 1;
-        avg = sum / numnum;
-    }
-    
-    System.out.println("Thx! Bye!");
-    System.out.println("Sum: " + sum);
-    System.out.println("Numbers: " + numnum);
-    System.out.println("Average: " + avg);
+        public static void printUntil(int numOfTimes) {
+        int n = 1;
+        while (n <= numOfTimes) {
+            System.out.println(n);
+            n++;
+        }
     }
 }
