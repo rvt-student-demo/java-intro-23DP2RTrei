ae77d6e887ad0c00b0cb1778165d5f610216e953
23DP2RTrei <166109672+23DP2RTrei@users.noreply.github.com>
2024-10-03 09:11:17 +0000

    Divisible by three

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,14 +6,20 @@ public class App
 {
 
         public static void main(String[] args){
-            printUntil(5); 
+            divByThree(2, 10); 
         }
 
-        public static void printUntil(int numOfTimes) {
-        int n = 1;
-        while (n <= numOfTimes) {
-            System.out.println(n);
-            n++;
-        }
+        public static void divByThree(int begin, int end) {
+            while (begin<=end) {
+                if (begin%3 == 0){
+                    System.out.println(begin);
+                }
+                else{
+                    begin++;
+                    continue;
+                }
+                begin += 3;
+            }
+        
     }
 }
