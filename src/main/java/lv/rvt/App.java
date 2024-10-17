144ffddbc02731d7d68f684483eee97d5d0c8940
23DP2RTrei <166109672+23DP2RTrei@users.noreply.github.com>
2024-10-17 09:35:18 +0000

    Exercise 3A — Improved Guessing Game

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,38 +1,49 @@
-package lv.rvt; //klases atrasanas vieta projekta
+package lv.rvt;
 
 import java.util.Random;
 import java.util.Scanner;
 
-public class App 
-{
+public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random rand = new Random();
-        int i = rand.nextInt(10);
+        int i = rand.nextInt(10) + 1; 
 
         System.out.println("I am thinking of a number from 1 to 10.");
         System.out.println("You must guess what it is in three tries.");
-        System.out.println("Enter a guess: ");
-        int n = 0;
-        int g = 0;
-        while (n != 3) {
-            int num = scanner.nextInt();
-            if (num != 1) {
-                System.out.println("Wrong!");
-                n++;
-            }
-            else{
-                System.out.println("Right!");
+
+        int tries = 0;
+        boolean won = false;
+
+        while (tries < 3) {
+            System.out.print("Enter a guess: ");
+            int guess = scanner.nextInt();
+            
+            if (guess == i) {
+                System.out.println("RIGHT!");
                 System.out.println("You have won the game!");
+                won = true;
+                break; 
+            } else {
+
+                int difference = Math.abs(i - guess);
+
+                if (difference >= 3) {
+                    System.out.println("cold");
+                } else if (difference == 2) {
+                    System.out.println("warm");
+                } else if (difference == 1) {
+                    System.out.println("hot");
+                }
+                
+                System.out.println("Wrong!");
             }
-            g++;
+            tries++;
         }
-        if (g == 3) {
-            System.out.println("The corect number was " + i + ".");
+
+        if (!won) {
+            System.out.println("The correct number was " + i + ".");
             System.out.println("You have lost the game!");
         }
-    
     }
-
-    
 }
