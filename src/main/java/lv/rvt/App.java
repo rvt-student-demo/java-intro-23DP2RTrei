0322e57d366f20f7b92cbbd940fa06733762bef0
23DP2RTrei <166109672+23DP2RTrei@users.noreply.github.com>
2024-10-17 09:21:15 +0000

    Exercise 2 — Guessing Game

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,26 +1,36 @@
 package lv.rvt; //klases atrasanas vieta projekta
 
-import java.util.*;
+import java.util.Random;
+import java.util.Scanner;
 
 public class App 
 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Integer> numbers = new ArrayList<>();
-        while (true) {
-            System.out.print("Ievadiet skaitli: ");
+        Random rand = new Random();
+        int i = rand.nextInt(10);
+
+        System.out.println("I am thinking of a number from 1 to 10.");
+        System.out.println("You must guess what it is in three tries.");
+        System.out.println("Enter a guess: ");
+        int n = 0;
+        int g = 0;
+        while (n != 3) {
             int num = scanner.nextInt();
-            if (num == 0) {
-                break; 
+            if (num != 1) {
+                System.out.println("Wrong!");
+                n++;
             }
-            numbers.add(num); 
+            else{
+                System.out.println("Right!");
+                System.out.println("You have won the game!");
+            }
+            g++;
         }
-        int sum = 0;
-        for (int number : numbers) {
-            sum += number;
-        } 
-        System.out.println("Summa ir: " + sum);
-    
+        if (g == 3) {
+            System.out.println("The corect number was " + i + ".");
+            System.out.println("You have lost the game!");
+        }
     
     }
 
