42f1cd6d2443e33017964ab3936127e209499289
23DP2RTrei <166109672+23DP2RTrei@users.noreply.github.com>
2024-11-06 12:03:30 +0000

    Persom update

--- src/main/java/lv/rvt/Person.java
+++ src/main/java/lv/rvt/Person.java
@@ -3,13 +3,29 @@ import java.util.*;
 public class Person {
     private String name;
     private int age;
+    private int weight;
+    private int height;
 
-    //Class constructor
-    public Person(String initialName){
-        this.name = initialName;
+    public Person(String initialName) {
         this.age = 0;
+        this.weight = 0;
+        this.height = 0;
+        this.name = initialName;
     }
-    public void printPerson(){
-        System.out.println(this.name + ", age " + this.age + " years");
+    public String getName() {
+        return this.name;
+    }
+
+    public void setHeight(int newHeight) {
+        this.height = newHeight;
+    }
+
+    public void setWeight(int newWeight) {
+        this.weight = newWeight;
+    }
+
+    public double bodyMassIndex() {
+        double heigthPerHundred = this.height / 100.0;
+        return this.weight / (heigthPerHundred * heigthPerHundred);
     }
 }
