90ea3cc34a49a0f74ab7e2723e12629a9a18da76
23DP2RTrei <166109672+23DP2RTrei@users.noreply.github.com>
2024-11-07 08:22:25 +0000

    Statistics pt2

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -13,15 +13,14 @@ public class App
 
         
     public static void main(String[] args) {
-         Statistics statistics = new Statistics();
+        Statistics statistics = new Statistics();
         statistics.addNumber(3);
         statistics.addNumber(5);
         statistics.addNumber(1);
         statistics.addNumber(2);
         System.out.println("Count: " + statistics.getCount());
-            
-        
-        
+        System.out.println("Sum: " + statistics.sum());
+        System.out.println("Average: " + statistics.average());
     }
 
 }
--- src/main/java/lv/rvt/Statistics.java
+++ src/main/java/lv/rvt/Statistics.java
@@ -2,16 +2,31 @@ package lv.rvt;
 
 public class Statistics {
     private int count;
+    private int sum;
 
     public Statistics() {
+        // initialize the variables count and sum here
         this.count = 0;
+        this.sum = 0;
     }
 
     public void addNumber(int number) {
-        this.count++;
+        this.sum += number;
+        this.count += 1;
     }
 
     public int getCount() {
         return this.count;
+    }
+
+    public int sum() {
+        return this.sum;
+    }
+
+    public double average() {
+        if (this.count == 0) {
+            return 0; 
+        }
+       return (double) this.sum / this.count; 
     }
 }
